4a5a8875bfac00f1785a08923423f5d6b54728e8
Thien Tran <gau.nernst@yahoo.com.sg>
2023-03-14 23:30:28 +0800

    add gelu, linear, transformerencoderlayer

--- transformer.py
+++ transformer.py
@@ -2,8 +2,14 @@ import numba as nb
 import numpy as np
 
 
-FLOAT_1D = nb.float32[::1]
-FLOAT_2D = nb.float32[:, ::1]
+FLOAT = nb.float32
+FLOAT_1D = FLOAT[::1]
+FLOAT_2D = FLOAT[:, ::1]
+
+
+@nb.vectorize([FLOAT(FLOAT)])
+def gelu(x):
+    return 0.5 * x * (1.0 + np.tanh(np.sqrt(2.0 / np.pi) * (x + 0.044715 * x * x * x)))
 
 
 @nb.njit(FLOAT_1D(FLOAT_1D), fastmath=True)
@@ -25,7 +31,7 @@ def softmax_2d_(x):
 
 
 @nb.njit(FLOAT_1D(FLOAT_1D, FLOAT_1D, FLOAT_1D, nb.float32), fastmath=True)
-def layernorm_1d_(x, w, b, eps):
+def layernorm_(x, w, b, eps):
     (M,) = x.shape
     sum_x = 0.0
     sum_x2 = 0.0
@@ -41,15 +47,25 @@ def layernorm_1d_(x, w, b, eps):
     return x
 
 
-@nb.njit(FLOAT_2D(FLOAT_2D, FLOAT_1D, FLOAT_1D, nb.float32), fastmath=True)
-def layernorm_2d_(x, w, b, eps):
-    for i in range(x.shape[0]):
-        layernorm_1d_(x[i], w, b, eps)
-    return x
+@nb.experimental.jitclass
+class LayerNorm:
+    w: FLOAT_1D
+    b: FLOAT_1D
+    eps: FLOAT
+
+    def __init__(self, emb_dim):
+        self.w = np.empty(emb_dim, dtype=FLOAT)
+        self.b = np.empty(emb_dim, dtype=FLOAT)
+        self.eps = 1e-5
+
+    def forward(self, x):
+        for i in range(x.shape[0]):
+            layernorm_(x[i], self.w, self.b, self.eps)
+        return x
 
 
 @nb.njit(FLOAT_2D(FLOAT_2D, FLOAT_2D, FLOAT_2D, nb.int64), fastmath=True)
-def self_attention(x, w_qkv, w_out, n_heads):
+def self_attention_(x, w_qkv, w_out, n_heads):
     # x: (seq_len, emb_dim)
     # w_qkv: (emb_dim x 3, emb_dim)
     # w_out: (emb_dim, emb_dim)
@@ -60,12 +76,74 @@ def self_attention(x, w_qkv, w_out, n_heads):
     q = qkv[:emb_dim]  # (emb_dim, seq_len) each
     k = qkv[emb_dim : emb_dim * 2]
     v = qkv[emb_dim * 2 :]
-    k *= 1.0 / np.sqrt(head_dim)
+    k /= np.sqrt(head_dim)
 
-    out = np.empty((emb_dim, seq_len), dtype=np.float32)
+    out = np.empty((emb_dim, seq_len), dtype=FLOAT)
     for i in range(n_heads):
         s = slice(head_dim * i, head_dim * (i + 1))
         attns = softmax_2d_(q[s].T @ k[s])  # (seq_len, seq_len)
         np.dot(v[s], attns.T, out[s])  # (head_dim, seq_len)
 
     return out.T @ w_out.T  # (seq_len, emb_dim)
+
+
+@nb.experimental.jitclass
+class Linear:
+    w: FLOAT_2D
+    b: FLOAT_1D
+
+    def __init__(self, in_dim, out_dim):
+        self.w = np.empty((out_dim, in_dim), dtype=FLOAT)
+        self.b = np.empty(out_dim, dtype=FLOAT)
+
+    def forward(self, x):
+        out = x @ self.w.T
+        np.add(out, self.b.reshape(1, -1), out)
+        return out
+
+
+@nb.experimental.jitclass
+class EncoderLayer:
+    w_qkv: FLOAT_2D
+    w_out: FLOAT_2D
+    n_heads: nb.int64
+    linear1: Linear
+    linear2: Linear
+    norm1: LayerNorm
+    norm2: LayerNorm
+
+    def __init__(self, emb_dim, n_heads):
+        self.w_qkv = np.empty((emb_dim * 3, emb_dim), dtype=FLOAT)
+        self.w_out = np.empty((emb_dim, emb_dim), dtype=FLOAT)
+        self.n_heads = n_heads
+        self.linear1 = Linear(emb_dim, emb_dim * 4)
+        self.linear2 = Linear(emb_dim * 4, emb_dim)
+        self.norm1 = LayerNorm(emb_dim)
+        self.norm2 = LayerNorm(emb_dim)
+
+    def forward(self, x):
+        x = x + self_attention_(self.norm1.forward(x.copy()), self.w_qkv, self.w_out, self.n_heads)
+        x = x + self.linear2.forward(gelu(self.linear1.forward(self.norm2.forward(x.copy()))))
+        return x
+
+
+def from_pytorch(nb_module, pt_module):
+    if isinstance(nb_module, LayerNorm):
+        nb_module.w[:] = pt_module.weight.detach().numpy()
+        nb_module.b[:] = pt_module.bias.detach().numpy()
+        nb_module.eps = pt_module.eps
+
+    elif isinstance(nb_module, Linear):
+        nb_module.w[:] = pt_module.weight.detach().numpy()
+        nb_module.b[:] = pt_module.bias.detach().numpy()
+
+    elif isinstance(nb_module, EncoderLayer):
+        nb_module.w_qkv[:] = pt_module.self_attn.in_proj_weight.detach().numpy()
+        nb_module.w_out[:] = pt_module.self_attn.out_proj.weight.detach().numpy()
+        nb_module.n_heads = pt_module.self_attn.num_heads
+        from_pytorch(nb_module.linear1, pt_module.linear1)
+        from_pytorch(nb_module.linear2, pt_module.linear2)
+        from_pytorch(nb_module.norm1, pt_module.norm1)
+        from_pytorch(nb_module.norm2, pt_module.norm2)
+
+    return nb_module
--- transformer_test.py
+++ transformer_test.py
@@ -1,15 +1,25 @@
 import numpy as np
 import torch
+import torch.nn.functional as F
 from torch import nn
 
-from transformer import layernorm_2d_, self_attention, softmax_2d_
+from transformer import EncoderLayer, LayerNorm, Linear, from_pytorch, gelu, self_attention_, softmax_2d_
+
+
+torch.set_grad_enabled(False)
 
 
 def assert_allclose(x, y):
     assert np.allclose(x, y, atol=1e-7)  # float32
 
 
-@torch.inference_mode()
+def test_gelu():
+    x = torch.randn(16, 12)
+    out_pt = F.gelu(x, approximate="tanh")
+    out_np = gelu(x.numpy())
+    assert_allclose(out_pt.numpy(), out_np)
+
+
 def test_softmax():
     x = torch.randn(16, 12)
     out_pt = torch.softmax(x, dim=1)
@@ -18,7 +28,6 @@ def test_softmax():
     assert_allclose(out_pt, out_nb)
 
 
-@torch.inference_mode()
 def test_layernorm():
     emb_dim = 32
     layer = nn.LayerNorm(emb_dim)
@@ -26,27 +35,54 @@ def test_layernorm():
 
     nn.init.normal_(layer.weight)
     nn.init.normal_(layer.bias)
-    out = layer(x)
+    out_pt = layer(x)
 
-    x_np = x.numpy()
-    w = layer.weight.numpy()
-    b = layer.bias.numpy()
-    out_nb = layernorm_2d_(x_np, w, b, layer.eps)
+    layer_nb = from_pytorch(LayerNorm(emb_dim), layer)
+    out_nb = layer_nb.forward(x.numpy())
 
-    assert_allclose(out.numpy(), out_nb)
+    assert_allclose(out_pt.numpy(), out_nb)
 
 
-@torch.inference_mode()
+def test_linear():
+    in_dim, out_dim = 16, 32
+    layer = nn.Linear(in_dim, out_dim)
+    x = torch.randn((4, in_dim))
+    out_pt = layer(x)
+
+    layer_nb = from_pytorch(Linear(in_dim, out_dim), layer)
+    out_nb = layer_nb.forward(x.numpy())
+
+    assert_allclose(out_pt.numpy(), out_nb)
+
+
 def test_self_attention():
     emb_dim, n_heads = 32, 4
     layer = nn.MultiheadAttention(emb_dim, n_heads, bias=False)
     x = torch.randn(4, emb_dim)
-    out, _ = layer(x, x, x, need_weights=False)
+    out_pt, _ = layer(x, x, x, need_weights=False)
 
     x_np = x.numpy()
     w_qkv = layer.in_proj_weight.numpy()
     w_out = layer.out_proj.weight.numpy()
+    out_nb = self_attention_(x_np, w_qkv, w_out, layer.num_heads)
 
-    out_nb = self_attention(x_np, w_qkv, w_out, layer.num_heads)
+    assert_allclose(out_pt.numpy(), out_nb)
 
-    assert_allclose(out.numpy(), out_nb)
+
+def test_encoder_layer():
+    emb_dim, n_heads = 32, 4
+    layer = nn.TransformerEncoderLayer(
+        emb_dim,
+        n_heads,
+        dim_feedforward=emb_dim * 4,
+        activation=nn.GELU(approximate="tanh"),
+        norm_first=True,
+    ).eval()
+    x = torch.randn(4, emb_dim)
+    out_pt = layer(x)
+
+    x_np = x.numpy()
+    layer_nb = from_pytorch(EncoderLayer(emb_dim, n_heads), layer)
+    out_nb = layer_nb.forward(x_np)
+
+    assert_allclose(out_pt.numpy(), out_nb)
